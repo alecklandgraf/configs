92948ca6a960a7809eb11686454e753258756553
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-07 13:11:11 -0700

    updates and adds install.sh

---
+++ keymap4macbook/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+keymap_file="/Users/$USER/Library/Application Support/KeyRemap4MacBook/private.xml"
+backup_file="/Users/$USER/Library/Application Support/KeyRemap4MacBook/private.xml_`eval date +%Y%m%d`"
+replacement_file="./private.xml"
+
+if [ -f $replacement_file]
+	then
+	echo "backing up original config file to $backup_file"
+	cp $keymap_file $backup_file
+	echo "installing new config file"
+	ln $replacement_file $keymap_file
+else
+	echo "please run install.sh in the same dir as private.xml"
+fi
