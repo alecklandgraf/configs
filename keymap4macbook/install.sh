e4d3113c243dc13295076a74d7308c9aec9082d5
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-04-07 13:36:03 -0700

    adds check if keyremap isn't installed

--- keymap4macbook/install.sh
+++ keymap4macbook/install.sh
@@ -3,6 +3,13 @@ keymap_file="/Users/$USER/Library/Application Support/KeyRemap4MacBook/private.x
 backup_file="/Users/$USER/Library/Application Support/KeyRemap4MacBook/private.xml_`eval date +%Y%m%d`"
 replacement_file="./private.xml"
 
+if [ ! -f $keymap_file ]
+	then
+	echo "looks like KeyRemap4MacBook is not installed"
+	echo "expected config file $keymap_file"
+	exit 0
+fi
+
 if [ -f $replacement_file ]
 	then
 	echo "backing up original config file to $backup_file"
