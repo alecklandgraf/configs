05b46c95a05226fd39455d1f4b4023dabdc4b630
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2015-12-08 17:09:19 -0800

    fishy

---
+++ config.fish
@@ -0,0 +1,25 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/alandgraf/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /Users/alandgraf/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/code
+export EDITOR=vim
+alias gs='git status'
+alias gd='git diff'
+alias gc='git commit -am'
+alias sp='./manage shell_plus'
+alias runserver='./manage.py runserver'
+
+# virtualfish like vitualenv, but for fish
+eval (python -m virtualfish auto_activation compat_aliases)
+
+function seedPostActivate --on-event virtualenv_did_activate:seed-3
+    set -gx DJANGO_SETTINGS_MODULE BE.settings.dev
+end
