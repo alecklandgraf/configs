18af222d500ef4057d727448dd9f1ad9f5b345c2
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2016-10-11 13:14:03 -0700

    adds autoenv.fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,13 +10,22 @@ source $OMF_PATH/init.fish
 
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/code
-export EDITOR=vim
+export EDITOR=subl
 alias gs='git status'
 alias gd='git diff'
 alias gc='git commit -am'
 alias sp='./manage.py shell_plus'
 alias runserver='./manage.py runserver'
 alias serve='python -m SimpleHTTPServer'
+alias c='python manage.py celery worker -B -l INFO'
+
+# docker support
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="/Users/alandgraf/.docker/machine/machines/default"
+export DOCKER_MACHINE_NAME="default"
+# Run this command to configure your shell:
+# eval $(docker-machine env default)
 
 function fuck -d "Correct your previous console command"
     set -l exit_code $status
@@ -36,10 +45,13 @@ end
 # virtualfish like vitualenv, but for fish
 eval (python -m virtualfish auto_activation compat_aliases)
 
-function seedPostActivate --on-event virtualenv_did_activate:seed-4
+function seedPostActivate --on-event virtualenv_did_activate:seed
     set -gx DJANGO_SETTINGS_MODULE BE.settings.dev
 end
 
-function auditronPostActivate --on-event virtualenv_did_activate:auditron2
+function auditronPostActivate --on-event virtualenv_did_activate:auditron
     set -gx DJANGO_SETTINGS_MODULE auditron.settings.dev
 end
+
+# load autoenv.fish
+. ~/.config/fish/autoenv.fish
